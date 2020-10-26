c568145a0348d3013f397748bcc20f262b8ca1ad
EC2 Default User <ec2-user@ip-172-31-36-77.ec2.internal>
2020-10-26 16:35:54 +0000

    adding unmocked unit for success test cases

---
+++ unmocked_unit_tests.py
@@ -0,0 +1,57 @@
+import sys
+sys.path.append("..")
+
+import unittest
+from responses import chat
+
+KEY_INPUT = "input"
+KEY_EXPECTED = "expected"
+
+class UnMockedTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.success_test_params = [
+            {
+                KEY_INPUT: "!!about",
+                KEY_EXPECTED: "This a little about me, im a chatbout but also i have a chatroom that communicate with other peopple"
+            },
+            {
+                KEY_INPUT: "!!help",
+                KEY_EXPECTED: "The commands i respond to are !! followed by either help,about,date,Whats my favorite color, covid, or funtranslate (your message here)"
+            },
+            {
+                KEY_INPUT: "!!Whats my favorite color",
+                KEY_EXPECTED: "BLue"
+            },
+            {
+                KEY_INPUT: "!!hello",
+                KEY_EXPECTED: "command not recognized"
+            },
+            {
+                KEY_INPUT: "!!funtranslate Good morning",
+                KEY_EXPECTED: " Bitchin' morning"
+            },
+        ]
+        
+    
+
+#---------------------------------------------------------------------------------------------------------------------------------------------
+
+    def test_parse_message_success(self):
+        chatbot = chat()
+
+        for test in self.success_test_params:
+            response = chatbot.response(test[KEY_INPUT])
+            expected = test[KEY_EXPECTED]
+            
+            self.assertEqual(response, expected)
+            
+            # Alternatively (and preferably), you can do self.assertDictEqual(response, expected)
+
+#---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    unittest.main()
